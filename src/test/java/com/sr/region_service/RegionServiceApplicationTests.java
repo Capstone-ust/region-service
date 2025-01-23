b994b5d5a390dbc3f637928ffbb830bd4cc5f355
SriramSriChandana <srichandanasriram2003@gmail.com>
2025-01-23 17:44:34 +0530

    Initial commit

---
+++ src/test/java/com/sr/region_service/RegionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sr.region_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
